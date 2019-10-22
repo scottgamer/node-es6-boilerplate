2f34d5f85995d6e65ca8c3ed8da7829533747007
Richard Munoz <richosojason@msn.com>
2019-10-22 00:45:45 -0500

    added opencageDataProvider

---
+++ src/http/providers/OpenCageDataProvider.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const getPlaces = async (query: string) => {
+  const key = process.env.OPEN_CAGE_DATA_KEY;
+  const url = `https://api.opencagedata.com/geocode/v1/geojson?q=${query}&key=${key}&limit=20&no_annotations=1`;
+  const response = await axios.get(url);
+  return JSON.parse(response.data);
+};
