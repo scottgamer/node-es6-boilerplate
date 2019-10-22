10e3fb3ad25e6d37e5c26eebbcdd0235d2fb6acc
Richard Muñoz <richard.munoz@crifa.com>
2019-10-22 16:44:47 -0500

    added createConnetion to server

--- src/server.ts
+++ src/server.ts
@@ -3,24 +3,32 @@ import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
 import errorHandlers from "./middleware/errorHandlers";
 import routes from "./routes";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 
-process.on("uncaughtException", e => {
-  console.log(e);
-  process.exit(1);
-});
+createConnection()
+  .then(async connection => {
+    process.on("uncaughtException", e => {
+      console.log(e);
+      process.exit(1);
+    });
 
-process.on("unhandledRejection", e => {
-  console.log(e);
-  process.exit(1);
-});
+    process.on("unhandledRejection", e => {
+      console.log(e);
+      process.exit(1);
+    });
 
-const router = express();
-applyMiddleware(middleware, router);
-applyRoutes(routes, router);
-applyMiddleware(errorHandlers, router);
+    const router = express();
+    applyMiddleware(middleware, router);
+    applyRoutes(routes, router);
+    applyMiddleware(errorHandlers, router);
 
-const { PORT = 3000 } = process.env;
+    const { PORT = 3000 } = process.env;
 
-router.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}...`);
-});
+    router.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
