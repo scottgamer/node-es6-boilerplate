174d6f6fc3f020edb6815af976ec2e5569b9fe4a
Richard Munoz <richosojason@msn.com>
2019-10-22 00:44:22 -0500

    added searchRoutes

---
+++ src/http/routes/searchRoutes.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { getPlacesByName } from "../controllers/SearchController";
+
+export default [
+  {
+    path: "/api/v1/search",
+    method: "get",
+    handler: [
+      async ({ query }: Request, res: Response) => {
+        const result = await getPlacesByName(query.q);
+        res.status(200).send(result);
+      }
+    ]
+  }
+];
