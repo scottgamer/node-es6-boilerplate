046c9615dc678ab919ad38fb62c73b9efefdce7f
Richard Muñoz <richard.munoz@crifa.com>
2019-10-22 17:13:43 -0500

    added user entity

---
+++ src/http/entities/User.ts
@@ -0,0 +1,44 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+  CreateDateColumn,
+  UpdateDateColumn
+} from "typeorm";
+// import { Length, IsNotEmpty } from "class-validator";
+import bcrypt from "bcryptjs";
+
+// TODO: fix decorators for typeorm
+
+@Entity()
+@Unique(["username"])
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
+
+  checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+    return bcrypt.compareSync(unencryptedPassword, this.password);
+  }
+}
