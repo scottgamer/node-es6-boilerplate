54fd6f428123f414842e870d8bfe949c00c5d355
Richard Muñoz <richard.munoz@crifa.com>
2019-10-22 16:45:24 -0500

    added helmet to middleware common

--- src/middleware/common.ts
+++ src/middleware/common.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import cors from "cors";
 import parser from "body-parser";
 import compression from "compression";
+import helmet from "helmet";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -16,4 +17,8 @@ export const handleBodyRequestParsing = (router: Router) => {
 
 export const handleCompression = (router: Router) => {
   router.use(compression());
+};
+
+export const handleHelmet = (router: Router)=>{
+  router.use(helmet());
 };
