4af9a7a34289ee71d2379cdc2cebe3f2d74bae9e
Richard Muñoz <richard.munoz@crifa.com>
2019-10-22 17:27:23 -0500

    added role and jwt middleware

---
+++ src/http/middleware/jwt.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import * as jwt from "jsonwebtoken";
+import config from "../config/config";
+
+export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization!;
+  let jwtPayload;
+
+  try {
+    jwtPayload = jwt.verify(token, config.jwtSecret);
+    res.locals.jwtPayload = jwtPayload;
+  } catch (error) {
+    res.status(401).send({ message: "Unauthenticated", error });
+    return;
+  }
+
+  // TODO: fix types
+
+  // token is valid for 1 hour
+  // send a new token on every request
+  const { userId, username } = jwtPayload;
+  const newToken = jwt.sign({ userId, username }, config.jwtSecret, {
+    expiresIn: "1h"
+  });
+  res.setHeader("token", newToken);
+
+  next();
+};
---
+++ src/http/middleware/role.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { getRepository } from "typeorm";
+
+import { User } from "../entities/User";
+
+export const checkRole = (roles: Array<string>) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    // get user id from previous midleware
+    const id = res.locals.jwtPayload.userId;
+
+    const userRepository = getRepository(User);
+    let user: User;
+    try {
+      user = await userRepository.findOneOrFail(id);
+    } catch (id) {
+      res.status(401).send("Unauthorized");
+    }
+
+    // TODO: fix user value assign
+
+    // check if array of authorized roles includes the user's role
+    if (roles.indexOf(user.role) > -1) next();
+    else res.status(401).send("Unauthorized");
+  };
+};
