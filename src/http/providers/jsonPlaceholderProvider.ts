ccd0524c67412d2ad3d582140a8b5653d46e989e
Richard Muñoz <richard.munoz@crifa.com>
2019-10-22 17:27:01 -0500

    added placeholder provider

---
+++ src/http/providers/jsonPlaceholderProvider.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const getUsers = async () => {
+  const url = process.env.JSON_PLACEHOLDER_ULR;
+  const response = await axios.get(`${url}/users`);
+  return response.data;
+};
