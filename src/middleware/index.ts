d2e2e047bd88d1f89d4b15bd6c62087764d23b9d
Richard Munoz <richosojason@msn.com>
2019-10-21 21:49:30 -0500

    added comment to  middleware/index.ts

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,7 +1,8 @@
-import {
-  handleCors,
-  handleBodyRequestParsing,
-  handleCompression
-} from "./common";
+/**
+ * Add more middleware functions            *
+ * and import them in ./middleware/index.ts *
+ */
+
+import { handleCors, handleBodyRequestParsing, handleCompression } from "./common";
 
 export default [handleCors, handleBodyRequestParsing, handleCompression];
