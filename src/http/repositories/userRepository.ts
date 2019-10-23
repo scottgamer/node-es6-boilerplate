106f1e8174e4bab155d3e3da7aba2daba68a4541
Richard Muñoz <richard.munoz@crifa.com>
2019-10-23 16:34:16 -0500

    added register and findbyUsername methods in user repository

--- src/http/repositories/userRepository.ts
+++ src/http/repositories/userRepository.ts
@@ -1,5 +1,6 @@
 import { getRepository } from "typeorm";
 import { User } from "../entities/User";
+import { HTTP422Error } from "../../utils/httpErrors";
 
 export const findById = async (id: number) => {
   const userRepository = getRepository(User);
@@ -7,5 +8,22 @@ export const findById = async (id: number) => {
   return user;
 };
 
+export const findByUsername = async (username: string) => {
+  const userRepository = getRepository(User);
+  const user = await userRepository.findOne({ where: { username } });
+  return user;
+};
 
+export const register = async (userData: User) => {
+  if (await findByUsername(userData.username)) {
+    throw new HTTP422Error("Username already exists");
+  }
 
+  const user = new User();
+  user.username = userData.username;
+  user.password = userData.password;
+  user.role = userData.role;
+  user.hashPassword();
+  const userRepository = getRepository(User);
+  await userRepository.save(user);
+};
