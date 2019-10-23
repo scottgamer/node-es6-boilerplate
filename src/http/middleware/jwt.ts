a8ec4ddbfc4ee55ebd95803b3b19391248a29636
Richard Muñoz <richard.munoz@crifa.com>
2019-10-23 14:38:43 -0500

    fixed jwt and roles middleware

--- src/http/middleware/jwt.ts
+++ src/http/middleware/jwt.ts
@@ -2,25 +2,23 @@ import { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
 import config from "../../config/config";
 
-export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization!;
-  let jwtPayload;
+// TODO: format using HTTPErrors
 
+export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
   try {
-    jwtPayload = jwt.verify(token, config.jwtSecret);
+    const token = req.headers.authorization;
+    const jwtPayload: any = jwt.verify(token, config.jwtSecret);
     res.locals.jwtPayload = jwtPayload;
+
+    // token is valid for 1 hour
+    // send a new token on every request
+    const { userId, username } = jwtPayload;
+    const newToken = jwt.sign({ userId, username }, config.jwtSecret, {
+      expiresIn: "1h"
+    });
+    res.setHeader("token", newToken);
+    next();
   } catch (error) {
     res.status(401).send({ message: "Unauthenticated", error });
-    return;
   }
-
-  // token is valid for 1 hour
-  // send a new token on every request
-  const { userId, username } = jwtPayload;
-  const newToken = jwt.sign({ userId, username }, config.jwtSecret, {
-    expiresIn: "1h"
-  });
-  res.setHeader("token", newToken);
-
-  next();
 };
--- src/http/middleware/role.ts
+++ src/http/middleware/role.ts
@@ -2,22 +2,28 @@ import { Request, Response, NextFunction } from "express";
 import { getRepository } from "typeorm";
 
 import { User } from "../entities/User";
+import { HTTP401Error } from "../../utils/httpErrors";
+
+// TODO: handle error using middleware
 
 export const checkRole = (roles: Array<string>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    // get user id from previous midleware
-    const id = res.locals.jwtPayload.userId;
-
-    const userRepository = getRepository(User);
-    let user: User;
     try {
+      // get userId from previous midleware
+      const id = res.locals.jwtPayload.userId;
+      const userRepository = getRepository(User);
+      let user: User;
       user = await userRepository.findOneOrFail(id);
-    } catch (id) {
-      res.status(401).send("Unauthorized");
+      // check if array of authorized roles includes the user's role
+      if (!(roles.indexOf(user.role) > -1)) {
+        throw new HTTP401Error("Unauthorized");
+      }
+      next(); 
+      // else {
+      //   res.status(401).send("Unauthorized");
+      // }
+    } catch (error) {
+      res.status(401).send(error);
     }
-
-    // check if array of authorized roles includes the user's role
-    if (roles.indexOf(user.role) > -1) next();
-    else res.status(401).send("Unauthorized");
   };
 };
