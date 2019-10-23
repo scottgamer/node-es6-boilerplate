2a0ff788ea10db85727f1c5f50b884056f30fe69
Richard Muñoz <richard.munoz@crifa.com>
2019-10-23 16:34:38 -0500

    added validator to auth routes

--- src/http/routes/authRoutes.ts
+++ src/http/routes/authRoutes.ts
@@ -1,11 +1,12 @@
 import * as authController from "../controllers/authController";
+import { validateRegister } from "../validators/authValidator";
 import { checkJwt } from "../middleware/jwt";
 
 export default [
   {
     path: "/api/v1/auth/register",
     method: "post",
-    handler: [authController.register]
+    handler: [validateRegister, authController.register]
   },
   {
     path: "/api/v1/auth/login",
