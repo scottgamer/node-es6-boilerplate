559b31a61ccf8e70ff22876d05dc5a8161e915c2
Richard Muñoz <richard.munoz@crifa.com>
2019-10-23 11:47:52 -0500

    added migration script to create admin user

---
+++ src/http/migrations/1571849091255-CreateAdminUser.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner, getRepository } from "typeorm";
+import {User, userRole} from "../entities/User";
+
+export class CreateAdminUser1571849091255 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    let user = new User();
+    user.username = "admin";
+    user.password = "admin";
+    user.hashPassword();
+    user.role = userRole.ADMIN;
+    const userRepository = getRepository(User);
+    await userRepository.save(user);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
