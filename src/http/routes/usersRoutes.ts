a85fb1fe06a201b47cf6f7802b1d2e2f92b6df01
Richard Muñoz <richard.munoz@crifa.com>
2019-10-23 14:38:23 -0500

    added authentication authorization middleware to userRoutes

--- src/http/routes/usersRoutes.ts
+++ src/http/routes/usersRoutes.ts
@@ -1,9 +1,11 @@
 import * as usersController from "../controllers/usersController";
+import { checkJwt } from "../middleware/jwt";
+import { checkRole } from "../middleware/role";
 
 export default [
   {
     path: "/api/v1/users",
     method: "get",
-    handler: [usersController.getUsers]
+    handler: [checkJwt, checkRole(["USER"]), usersController.getUsers]
   }
 ];
