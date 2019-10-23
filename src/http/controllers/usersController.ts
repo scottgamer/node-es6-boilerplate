ba2c2a9b5ac6cbedebeb8f89e7626a4b6579305a
Richard Muñoz <richard.munoz@crifa.com>
2019-10-23 16:33:22 -0500

    chnaged service in user controller

--- src/http/controllers/usersController.ts
+++ src/http/controllers/usersController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import * as jsonPlaceholderProvider from "../providers/jsonPlaceholderProvider";
+import * as jsonPlaceholderProvider from "../services/jsonPlaceholderService";
 
 export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
   const response = await jsonPlaceholderProvider.getUsers();
