b8a252b9d901a8beec7638665925dbac77c93d76
Richard Muñoz <richard.munoz@crifa.com>
2019-10-23 16:30:22 -0500

    added auth controller

---
+++ src/http/controllers/authController.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import * as authProcess from "../processes/authProcess";
+import { User } from "../entities/User";
+import { clientError } from "../../utils/ErrorHandler";
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data: User = req.body;
+    const response = await authProcess.register(data);
+    res.status(200).send(response);
+  } catch (error) {
+    clientError(error, res, next);
+  }
+};
+
+// TODO: implement login
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  // res.status(200).send(response);
+};
+
+// TODO: implement change password
+export const changePassword = async (req: Request, res: Response, next: NextFunction) => {
+  // res.status(200).send(response);
+};
