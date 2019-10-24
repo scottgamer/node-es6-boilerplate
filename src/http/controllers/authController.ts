4eeac200229ed7cf2efa26351d41cb64353f4b2a
Richard Muñoz <richard.munoz@crifa.com>
2019-10-24 17:07:32 -0500

    added change pasword to auth controller

--- src/http/controllers/authController.ts
+++ src/http/controllers/authController.ts
@@ -15,7 +15,7 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
     const response = await authProcess.login(username, password);
     res.status(200).send(response);
   } catch (error) {
@@ -23,7 +23,12 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
   }
 };
 
-// TODO: implement change password
 export const changePassword = async (req: Request, res: Response, next: NextFunction) => {
-  // res.status(200).send(response);
+  try {
+    const { password, newPassword } = req.body;
+    const response = await authProcess.changePassword(res, password, newPassword);
+    res.status(200).send(response);
+  } catch (error) {
+    clientError(error, res, next);
+  }
 };
