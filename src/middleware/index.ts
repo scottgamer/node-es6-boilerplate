1faa1ec7f6b9d0600e71d10c1d1223ae3360cfdb
Richard Muñoz <richard.munoz@crifa.com>
2019-10-24 11:06:50 -0500

    added swagger

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,15 @@
-/**
- * Add more middleware functions            *
+/********************************************
+ * Add here more middleware functions       *
  * and import them in ./middleware/index.ts *
- */
+ *******************************************/
 
 import { handleCors, handleBodyRequestParsing, handleCompression, handleHelmet } from "./common";
+import { handleAPIDocs } from "./apiDocs";
 
-export default [handleCors, handleBodyRequestParsing, handleCompression, handleHelmet];
+export default [
+  handleCors,
+  handleBodyRequestParsing,
+  handleCompression,
+  handleHelmet,
+  handleAPIDocs
+];
