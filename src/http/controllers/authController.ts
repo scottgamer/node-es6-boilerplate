28769b582596b08de9097f762f95d27fc35a44cf
Richard Muñoz <richard.munoz@crifa.com>
2019-10-24 12:35:47 -0500

    fixed var name

--- src/http/controllers/authController.ts
+++ src/http/controllers/authController.ts
@@ -5,8 +5,8 @@ import { clientError } from "../../utils/ErrorHandler";
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const data: User = req.body;
-    const response = await authProcess.register(data);
+    const user: User = req.body;
+    const response = await authProcess.register(user);
     res.status(200).send(response);
   } catch (error) {
     clientError(error, res, next);
