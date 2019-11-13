309ca3cce5f7e0364ba7fc8fb046b3ed65fca6b7
Richard Muñoz <richard.munoz@crifa.com>
2019-11-13 14:42:18 -0500

    changed to default import jwt

--- src/http/middleware/jwt.ts
+++ src/http/middleware/jwt.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import * as jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import config from "../../config/config";
 
 export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
